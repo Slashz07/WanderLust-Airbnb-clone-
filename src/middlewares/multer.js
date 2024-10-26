29d2d4e00886e358ae3893838c438a03f28153bf
Sumit saini <sumitforsaken76248@gmail.com>
2024-10-26 10:39:51 +0530

    typo corrected

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,7 +4,7 @@ const storage=multer.diskStorage({
     destination:(req,file,cb)=>{
         cb(null,'./src/public/uploadedImages')
     },
-    fulename:function (req,file,cb){
+    filename:function (req,file,cb){
     const uniqueName=Date.now() + '-' + Math.round(Math.random()* 1E9)
     cb(null,file.fieldname)
     }
