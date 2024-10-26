4841bf333c5ee9f68465a367d9f70f0e1200ddde
Sumit saini <sumitforsaken76248@gmail.com>
2024-10-26 08:33:39 +0530

    basic formatting

--- src/routes/listing.route.js
+++ src/routes/listing.route.js
@@ -1,43 +1,52 @@
-import {Router} from "express"
+import { Router } from "express"
 import { isOwner, isLoggedIn } from "../middlewares/login.js"
-import { getAllListing,
-         getListingInfo,
-         createNewProp,
-         saveNewProp,
-         editProp,
-         saveUpdates,
-         deleteProp,
-         pageNotFound,
-         getCategorisedListings,
-         searchListings,
-        } from "../controllers/listings.controller.js"
+import {
+        getAllListing,
+        getListingInfo,
+        createNewProp,
+        saveNewProp,
+        editProp,
+        saveUpdates,
+        deleteProp,
+        pageNotFound,
+        getCategorisedListings,
+        searchListings,
+} from "../controllers/listings.controller.js"
+
 import { upload } from "../middlewares/multer.js"
 
-const router=Router()
+const router = Router()
 
 router.route("/search").post(searchListings)
 
 router.route("/").get(getAllListing)
+
 router.route("/new").get(
         isLoggedIn,
         createNewProp)
+
 router.route("/").post(
         isLoggedIn,
         upload.single("image"),
         saveNewProp)
+
 router.route("/:id").get(getListingInfo)
+
 router.route("/:id/edit").get(
         isLoggedIn,
         isOwner,
         editProp)
+
 router.route("/:id").patch(
         isLoggedIn,
         isOwner,
         upload.single("image"),
         saveUpdates)
+
 router.route("/category/:type").get(
         getCategorisedListings
 )
+
 router.route("/:id/delete").delete(
         isLoggedIn,
         isOwner,
